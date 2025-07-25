94c1ea5d96d0ff5439c6751f3a492ca205c3bc98
kailzxh <137093796+kailzxh@users.noreply.github.com>
2025-07-25 15:50:58 +0530

    assignment 2 and 3 completed

---
+++ Week-6-React/ReactJS-HOL-2/code/studentapp/src/components/Contact.js
@@ -0,0 +1,14 @@
+
+import React, { Component } from 'react';
+
+class Contact extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Contact Us at studentportal@example.com</h3>
+      </div>
+    );
+  }
+}
+
+export default Contact;
